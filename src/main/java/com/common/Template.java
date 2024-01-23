eac61fd504af302548f7dadff77b13b692aaa0e0
miniato2 <whgg456@gmail.com>
2024-01-24 00:31:30 +0900

    1

--- src/main/java/com/common/Template.java
+++ src/main/java/com/common/Template.java
@@ -1,7 +1,26 @@
 package com.common;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Template {
+    private static SqlSessionFactory sqlSessionFactory;
 
-    // * 주석을 지우고 sqlSession을 생성하는 공통 template 파일을 작성하세요.
-
+    public static SqlSession getSqlsession(){
+        if(sqlSessionFactory == null){
+            String resource = "config/mybatis-config.xml";
+            try {
+                InputStream inputStream = Resources.getResourceAsStream(resource);
+                sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sqlSessionFactory.openSession(false);
+    }
 }
--- src/main/java/com/management/product/view/ProductPrint.java
+++ src/main/java/com/management/product/view/ProductPrint.java
@@ -7,36 +7,42 @@ import java.util.List;
 
 public class ProductPrint {
 
-    // * 모든 출력은 System.out.println()을 통해 콘솔에 출력하는 것을 의미합니다.
-    // 주석을 지우고 다음 네 가지 메소드를 작성하세요.
-
     public void printAllProductList(List<ProductDTO> allProductList) {
-
-        // 1. 전체 조회한 목록 출력하는 메소드
-        //    (조건) List<ProductDTO>로 받아온 데이터 목록을 전체 출력하세요.
-
+        for(ProductDTO product : allProductList){
+            System.out.println(product);
+        }
     }
 
     public void printProductList(List<ProductDTO> productList, SearchCondition searchCondition) {
+        System.out.println("검색조건 : " + searchCondition.getOption());
+        System.out.println("검색어 : " + searchCondition.getValue());
 
-        // 2. 조건에 따라 조회한 목록을 출력하는 메소드
-        //    (조건 1) SearchCondition 객체로 검색 조건이 무엇인지 출력하세요.
-        //    (조건 2) List<ProductDTO>로 받아온 데이터 목록을 전체 출력하세요.
-
+        for(ProductDTO product : productList){
+            System.out.println(product);
+        }
     }
 
     public void printSuccessMessage(String successCode) {
+        String successMessage = "";
 
-        // 3. 성공메시지를 출력하는 메소드
-        //    (조건) 성공코드를 전달받아 성공을 알리는 메시지를 출력하세요.
-
+        switch (successCode){
+            case "insertProduct" : successMessage = "추가 성공"; break;
+            case "updateProduct" : successMessage = "변경 성공"; break;
+            case "deleteProduct" : successMessage = "삭제 성공"; break;
+        }
+        System.out.println(successMessage);
     }
 
     public void printErrorMessage(String errorCode) {
+        String errorMessage = "";
 
-        // 4. 에러메시지를 출력하는 메소드
-        //    (조건) 에러코드를 전달받아 에러를 알리는 메시지를 출력하세요.
-
+        switch(errorCode){
+            case "selectAllList" : errorMessage = "조회 실패"; break;
+            case "searchCondition" : errorMessage = "조건에 맞는 결과가 없습니다."; break;
+            case "insertProduct" : errorMessage = "추가 실패"; break;
+            case "updateProduct" : errorMessage = "변경 실패"; break;
+            case "deleteProduct" : errorMessage = "삭제 실패"; break;
+        }
+        System.out.println(errorMessage);
     }
-
 }
