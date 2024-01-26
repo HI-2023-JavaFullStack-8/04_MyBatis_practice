1126ee79743f5210b057f0e7763d51bfee930d1b
daeun100299 <goode439767@gmail.com>
2024-01-26 18:03:02 +0900

    MyBatis 실습

--- src/main/java/com/management/category/model/dao/CategoryDAOProvider.java
+++ src/main/java/com/management/category/model/dao/CategoryDAOProvider.java
@@ -1,46 +1,59 @@
 package com.management.category.model.dao;
 
 import com.management.category.model.dto.CategoryDTO;
+import org.apache.ibatis.jdbc.SQL;
 
 import java.util.Map;
 
 public class CategoryDAOProvider {
 
-    // * 주석을 지우고 Provider 활용하여 내용을 작성하세요.
-
     public String selectCategoryList(Map<String, String> parameter) {
 
-        // 1. Provider를 활용하여 제품분류 목록을 조회하는 코드를 작성하세요.
-        //    (조건) 전체 조회 or 제품분류 순위로 정렬해 조회하는 조건에 따른 Dynamic SQL로 작성하세요.
-        //          제품분류 순위는 PRODUCT_INFO 테이블과 JOIN하여, 해당 제품분류에 속한 제품 갯수가 많은 순입니다.
-        //          예를 들어, PRODUCT_INFO 테이블에 제품분류가 A인 제품이 10개, B인 제품이 4개, C인 제품이 13개 있다면
-        //          제품분류 순위는 C > A > B 입니다. (단, 제품 갯수가 0개인 제품분류는 목록에 출력되지 않아도 됩니다.)
-        //    아래 작성된 return null은 과제 툴 오류를 제거하고자 임의 작성하였으니 지우고 로직을 작성하세요.
-        return null;
+        SQL sql = new SQL()
+                .SELECT("c.category_code");
+        if(parameter.get("option") == "allList") {
+            sql.SELECT("c.category_name");
+        }
+        if(parameter.get("option") == "orderList") {
+            sql.SELECT("c.category_name, COUNT(*) AS 'count'");
+        }
+        sql.FROM("product_category c");
+        if(parameter.get("option") == "orderList") {
+            sql.JOIN("product_info i ON c.category_code = i.category_code")
+                    .GROUP_BY("i.category_code")
+                    .ORDER_BY("count");
+        }
+
+        return sql.toString();
 
     }
 
     public String insertCategory(CategoryDTO category) {
 
-        // 2. Provider를 활용하여 제품분류를 등록하는 코드를 작성하세요.
-        //    아래 작성된 return null은 과제 툴 오류를 제거하고자 임의 작성하였으니 지우고 로직을 작성하세요.
-        return null;
+        return new SQL()
+                .INSERT_INTO("product_category")
+                .VALUES("category_name", "#{categoryName}")
+                .toString();
 
     }
 
     public String updateCategory(CategoryDTO category) {
 
-        // 3. Provider를 활용하여 제품분류명을 수정하는 코드를 작성하세요.
-        //    아래 작성된 return null은 과제 툴 오류를 제거하고자 임의 작성하였으니 지우고 로직을 작성하세요.
-        return null;
+        SQL sql = new SQL()
+                .UPDATE("product_category")
+                .SET("category_name = #{categoryName}")
+                .WHERE("category_code = #{categoryCode}");
+
+        return sql.toString();
 
     }
 
     public String deleteCategory(Map<String, String> parameter) {
 
-        // 4. Provider를 활용하여 제품분류를 삭제하는 코드를 작성하세요.
-        //    아래 작성된 return null은 과제 툴 오류를 제거하고자 임의 작성하였으니 지우고 로직을 작성하세요.
-        return null;
+        return new SQL()
+                .DELETE_FROM("product_category")
+                .WHERE("category_code = #{categoryCode}")
+                .toString();
 
     }
 }
--- src/main/java/com/management/product/view/ProductPrint.java
+++ src/main/java/com/management/product/view/ProductPrint.java
@@ -7,35 +7,53 @@ import java.util.List;
 
 public class ProductPrint {
 
-    // * 모든 출력은 System.out.println()을 통해 콘솔에 출력하는 것을 의미합니다.
-    // 주석을 지우고 다음 네 가지 메소드를 작성하세요.
-
     public void printAllProductList(List<ProductDTO> allProductList) {
 
-        // 1. 전체 조회한 목록 출력하는 메소드
-        //    (조건) List<ProductDTO>로 받아온 데이터 목록을 전체 출력하세요.
+        System.out.println("========== 제품 전체 조회 목록 ==========");
+        for(ProductDTO list : allProductList) {
+            System.out.println(list);
+        }
 
     }
 
     public void printProductList(List<ProductDTO> productList, SearchCondition searchCondition) {
 
-        // 2. 조건에 따라 조회한 목록을 출력하는 메소드
-        //    (조건 1) SearchCondition 객체로 검색 조건이 무엇인지 출력하세요.
-        //    (조건 2) List<ProductDTO>로 받아온 데이터 목록을 전체 출력하세요.
+        System.out.println("========== " + "조건부 제품 목록" + " ==========");
+        System.out.println("검색 조건 : " + searchCondition.getOption());
+        System.out.println("-----------------------------------");
+        for(ProductDTO list : productList) {
+            System.out.println(list);
+        }
 
     }
 
     public void printSuccessMessage(String successCode) {
 
-        // 3. 성공메시지를 출력하는 메소드
-        //    (조건) 성공코드를 전달받아 성공을 알리는 메시지를 출력하세요.
+        String successMessage = "";
+
+        switch(successCode) {
+            case "registNewProduct" : successMessage = "새로운 제품 등록을 성공했습니다."; break;
+            case "modifyProductionInfo" : successMessage = "제품 수정을 성공했습니다."; break;
+            case "deleteProduct" : successMessage = "제품 삭제를 성공했습니다."; break;
+        }
+
+        System.out.println(successMessage);
 
     }
 
     public void printErrorMessage(String errorCode) {
 
-        // 4. 에러메시지를 출력하는 메소드
-        //    (조건) 에러코드를 전달받아 에러를 알리는 메시지를 출력하세요.
+        String errorMessage = "";
+
+        switch(errorCode) {
+            case "selectAllProductList" : errorMessage = "전체 메뉴 조회를 실패했습니다."; break;
+            case "selectProductByCondition" : errorMessage = "조건에 부합한 목록 조회를 실패했습니다."; break;
+            case "registNewProduct" : errorMessage = "새로운 제품 등록을 실패했습니다."; break;
+            case "modifyProductionInfo" : errorMessage = "제품 수정을 실패했습니다."; break;
+            case "deleteProduct" : errorMessage = "제품 삭제를 성공했습니다."; break;
+        }
+
+        System.out.println(errorMessage);
 
     }
 
